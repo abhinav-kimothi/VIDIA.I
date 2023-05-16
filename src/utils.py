9eb844a8f13d4ad4a875f4b443759d335c54950e
Abhinav Kimothi <118165775+abhinav-kimothi@users.noreply.github.com>
2023-05-16 10:51:39 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -133,9 +133,7 @@ def extract_YT(link):
     text=str(loader.load()[0])
     words=len(text.split())
     num=1
-    st.write(text)
-    st.write(words)
-    st.write(num)
+
     return words, num, text
 
 #### Clear data upon new input
